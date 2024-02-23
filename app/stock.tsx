6318a0f5e9ae068e6b1d135ccc4f37a1ff171024
anilyavas <anil.yavas99@gmail.com>
2024-02-23 14:36:18 +0300

    stock details screen onpress events

---
+++ app/stock.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from '@/components/Themed';
+
+const StockDetails = () => {
+  return (
+    <View>
+      <Text>stock</Text>
+    </View>
+  );
+};
+
+export default StockDetails;
--- components/StockListItem.tsx
+++ components/StockListItem.tsx
@@ -1,8 +1,9 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { Text, View } from './Themed';
 import Colors from '@/constants/Colors';
 import { MonoText } from './StyledText';
+import { Link } from 'expo-router';
 
 type Stock = {
   name: string;
@@ -17,23 +18,25 @@ const StockListItem = ({ stock }: StockListItem) => {
   const change = Number.parseFloat(stock.percent_change);
   const close = Number.parseFloat(stock.close);
   return (
-    <View style={styles.container}>
-      <View style={{ flexDirection: 'row', gap: 10, paddingBottom: 5 }}>
-        <Text style={styles.symbol}>
-          {stock.symbol} <AntDesign name='staro' size={18} color={'grey'} />
-        </Text>
-        <View style={{ flex: 1 }} />
-        <MonoText style={{}}>${close.toFixed(1)}</MonoText>
-      </View>
-      <View style={{ flexDirection: 'row', gap: 10, paddingBottom: 5 }}>
-        <Text style={styles.brandName}>{stock.name}</Text>
-        <View style={{ flex: 1 }} />
-        <MonoText style={{ color: change > 0 ? 'green' : 'red' }}>
-          {change > 0 ? '+' : ''}
-          {change.toFixed(1)}%
-        </MonoText>
-      </View>
-    </View>
+    <Link href={'/stock'} asChild>
+      <Pressable style={styles.container}>
+        <View style={{ flexDirection: 'row', gap: 10, paddingBottom: 5 }}>
+          <Text style={styles.symbol}>
+            {stock.symbol} <AntDesign name='staro' size={18} color={'grey'} />
+          </Text>
+          <View style={{ flex: 1 }} />
+          <MonoText style={{}}>${close.toFixed(1)}</MonoText>
+        </View>
+        <View style={{ flexDirection: 'row', gap: 10, paddingBottom: 5 }}>
+          <Text style={styles.brandName}>{stock.name}</Text>
+          <View style={{ flex: 1 }} />
+          <MonoText style={{ color: change > 0 ? 'green' : 'red' }}>
+            {change > 0 ? '+' : ''}
+            {change.toFixed(1)}%
+          </MonoText>
+        </View>
+      </Pressable>
+    </Link>
   );
 };
 
