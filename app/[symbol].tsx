9fc83c84cb401734f3828a65400dfe5bb0569267
anilyavas <anil.yavas99@gmail.com>
2024-02-25 12:32:49 +0300

    query stock details

--- app/[symbol].tsx
+++ app/[symbol].tsx
@@ -1,21 +1,37 @@
 import { View, Text } from '@/components/Themed';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import top5 from '../assets/data/top5.json';
-import StockListItem from '@/components/StockListItem';
 import Graph from '@/components/Graph';
+import { useQuery, gql } from '@apollo/client';
+import { ActivityIndicator } from 'react-native';
 
+const query = gql`
+  query MyQuery($symbol: String) {
+    quote(symbol: $symbol) {
+      symbol
+      name
+      percent_change
+      close
+    }
+  }
+`;
 const StockDetails = () => {
   const { symbol } = useLocalSearchParams();
-  const stock = top5[symbol];
-  if (!stock) {
+  const { data, loading, error } = useQuery(query, {
+    variables: { symbol },
+  });
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
     return <Text>Stock with symbol {symbol} could not be found</Text>;
   }
+  const stock = data.quote;
   return (
     <View style={{ padding: 10 }}>
       <Stack.Screen
         options={{ title: stock.symbol, headerBackTitleVisible: false }}
       />
-      <StockListItem stock={stock} />
       <Graph />
     </View>
   );
